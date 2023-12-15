3af883eadf2ee1c738ba25d236b9302c056ee250
Maykon Granemann <maykon@zuplae.com>
2023-12-15 17:30:36 -0300

    Flask forms e listagens

--- Aulas/Aula0004/main.py
+++ Aulas/Aula0004/main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+lista_produtos = []
+
 
 @app.route("/")
 def inicio():
@@ -10,8 +12,19 @@ def inicio():
 def create():
     return render_template('create.html')
 
+@app.route('/save', methods=["POST"])
+def save():
+    nome = request.form.get("nome")
+    descricao = request.form.get("descricao")
+    valor = request.form.get("valor")
+    prod = {"nome":nome, "descricao": descricao, "valor": valor}
+    lista_produtos.append(prod)
+    return redirect('/list')
+
+
 @app.route("/list")
 def list():
-    return render_template('list.html')
+    return render_template('list.html', lista = lista_produtos)
 
 app.run(debug=True)
+
