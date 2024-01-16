22fa16f672f137cd8b214e3e65ed71df9c46cb8f
Maykon Granemann <maykon@zuplae.com>
2024-01-16 13:25:09 -0300

    Aula 0007 - Repository

--- Aulas/Aula0007/repository.py
+++ Aulas/Aula0007/repository.py
@@ -1,12 +1,12 @@
 from pessoa import Pessoa
 
 def salvar(pessoa):
-    with open('pessoas.txt', 'a') as arquivo:
+    with open('arquivos/pessoas.txt', 'a') as arquivo:
         arquivo.write(str(pessoa) + '\n')
 
 def listar():
     lista_pessoas = []
-    with open('pessoas.txt', 'r') as arquivo:
+    with open('arquivos/pessoas.txt', 'r') as arquivo:
         for p in arquivo:
             p = p.strip() # retira \n
             dados = p.split(';') # quebra a linha em um lista onde cada possicao é um dado 
